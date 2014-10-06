123e243270fa20f8bc94f901792fe2e8bb8bfda1
Hoten <cjamcl@gmail.com>
2014-10-05 20:19:22 -0500

    monsters data

--- Unity/Assets/Scripts/Content/ContentManager.cs
+++ Unity/Assets/Scripts/Content/ContentManager.cs
@@ -10,17 +10,27 @@ namespace Gridia
         public static ContentManager Singleton { get; set; } //smell?
 
         public int ItemCount { get { return items.Count; } }
+        public int MonsterCount { get { return monsters.Count; } }
+        
         private readonly List<Item> items;
+        private readonly List<Monster> monsters;
         
         public ContentManager ()
         {
-            items = new ContentLoader<Item> ().Load ("content/items");
+            items = new ContentLoader<Item>().Load("content/items");
+            monsters = new ContentLoader<Monster>().Load("content/monsters");
+            MonoBehaviour.print(monsters[121].Name);
             Singleton = this; //smell?
         }
 
-        public Item GetItem (int id)
+        public Item GetItem(int id)
         {
-            return items [id];
+            return items[id];
+        }
+
+        public Monster GetMonster(int id)
+        {
+            return monsters[id];
         }
     }
 }
---
+++ Unity/Assets/Scripts/Content/Monster.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster {
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
