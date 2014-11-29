1ec3136ee0c8911ae9e202a4079f283460af063a
Connor Clark <cjamcl@gmail.com>
2014-11-29 03:18:14 -0600

    Floor decides movement speed

--- Client/Assets/Scripts/PlayerMovementState.cs
+++ Client/Assets/Scripts/PlayerMovementState.cs
@@ -8,7 +8,7 @@ namespace Gridia
         private Creature Player { get { return Locator.Get<TileMapView>().Focus; } }
         private long _cooldownUntil;
 
-        // : (
+        // :(
         private long getSystemTime()
         {
             return DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond - GridiaConstants.SERVER_TIME_OFFSET;
@@ -25,7 +25,34 @@ namespace Gridia
                     var pos = Player.Position;
                     var newPosition = pos + delta;
                     var now = getSystemTime();
-                    _cooldownUntil = now + 100;
+                    var baseTime = 200;
+                    
+                    // :(
+                    float movementModifier;
+                    var destination = Locator.Get<GridiaGame>().view.Focus.Position + delta;
+                    var floor = Locator.Get<GridiaGame>().tileMap.GetTile((int)destination.x, (int)destination.y, (int)destination.z).Floor;
+                    switch (floor) 
+                    {
+                        case 1:
+                            movementModifier = 0.3f;
+                            break;
+                        case 21:
+                        case 47:
+                            movementModifier = 0.8f;
+                            break;
+                        case 2:
+                        case 11:
+                        case 18:
+                        case 46:
+                            movementModifier = 1.5f;
+                            break;
+                        default:
+                            movementModifier = 1f;
+                            break;
+                    }
+                    
+                    var timeForMovement = baseTime / movementModifier;
+                    _cooldownUntil = now + (int)timeForMovement;
                     Player.AddPositionSnapshot(pos, now - Creature.RENDER_DELAY);
                     Player.AddPositionSnapshot(newPosition, _cooldownUntil - Creature.RENDER_DELAY);
                 }
