1bc827fac43ab45a054abc45606438dcd1263c94
Connor Clark <cjamcl@gmail.com>
2015-03-14 21:34:46 -0500

    bug fix in ScriptableUsageProcessing

--- Server/GridiaServer/src/main/groovy/com/hoten/gridia/scripting/ScriptableUsageProcessing.groovy
+++ Server/GridiaServer/src/main/groovy/com/hoten/gridia/scripting/ScriptableUsageProcessing.groovy
@@ -5,6 +5,7 @@ import com.hoten.gridia.content.ItemUse
 import com.hoten.gridia.content.ItemUseException
 import com.hoten.gridia.content.UsageProcessor
 import com.hoten.gridia.content.UsageProcessor.UsageResult
+import com.hoten.gridia.serving.ServingGridia
 
 class ScriptableUsageProcessing extends UsageProcessor {
     def EventDispatcher eventDispatcher
@@ -16,7 +17,7 @@ class ScriptableUsageProcessing extends UsageProcessor {
     
     def void validate(UsageResult result, ItemUse usage, ItemWrapper tool, ItemWrapper focus) {
         super.validate(result, usage, tool, focus)
-        def issues = eventDispatcher.dispatch("ValidateItemUse", null, [
+        def issues = eventDispatcher.dispatch("ValidateItemUse", ServingGridia.instance.worldEntity, [
                 result:result,
                 usage:usage,
                 tool:tool,
@@ -29,7 +30,7 @@ class ScriptableUsageProcessing extends UsageProcessor {
     
     def void implementResult(UsageResult result, ItemUse usage, ItemWrapper tool, ItemWrapper focus) {
         super.implementResult(result, usage, tool, focus)
-        eventDispatcher.dispatch("CompleteItemUse", null, [
+        eventDispatcher.dispatch("CompleteItemUse", ServingGridia.instance.worldEntity, [
                 result:result,
                 usage:usage,
                 tool:tool,
