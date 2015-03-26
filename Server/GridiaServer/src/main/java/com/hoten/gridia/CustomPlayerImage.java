bda0b55ca54ae9d156333116df8008cc02ef2201
Connor Clark <cjamcl@gmail.com>
2015-03-26 14:16:34 -0500

    refactored CustomPlayerImage

--- Server/GridiaServer/src/main/java/com/hoten/gridia/CustomPlayerImage.java
+++ Server/GridiaServer/src/main/java/com/hoten/gridia/CustomPlayerImage.java
@@ -6,8 +6,11 @@ import com.hoten.gridia.content.ItemInstance;
 public class CustomPlayerImage implements CreatureImage {
 
     public int bareHead, bareArms, bareChest, bareLegs;
-
     public int head, arms, chest, legs, shield, weapon;
+
+    private int getWearImage(ItemInstance item, int defaultValue) {
+        return item.isNothing() ? defaultValue : item.getItem().wearImage;
+    }
 
     public void moldToEquipment(Container equipment) {
         ItemInstance headEquipment = equipment.get(Item.ArmorSpot.Head.ordinal());
@@ -15,31 +18,10 @@ public class CustomPlayerImage implements CreatureImage {
         ItemInstance legsEquipment = equipment.get(Item.ArmorSpot.Legs.ordinal());
         ItemInstance shieldEquipment = equipment.get(Item.ArmorSpot.Shield.ordinal());
         ItemInstance weaponEquipment = equipment.get(Item.ArmorSpot.Weapon.ordinal());
-
-        if (headEquipment != ItemInstance.NONE) {
-            head = headEquipment.getItem().wearImage;
-        } else {
-            head = bareHead;
-        }
-        if (chestEquipment != ItemInstance.NONE) {
-            chest = chestEquipment.getItem().wearImage;
-        } else {
-            chest = bareChest;
-        }
-        if (legsEquipment != ItemInstance.NONE) {
-            legs = legsEquipment.getItem().wearImage;
-        } else {
-            legs = bareLegs;
-        }
-        if (shieldEquipment != ItemInstance.NONE) {
-            shield = shieldEquipment.getItem().wearImage;
-        } else {
-            shield = 0;
-        }
-        if (weaponEquipment != ItemInstance.NONE) {
-            weapon = weaponEquipment.getItem().wearImage;
-        } else {
-            weapon = 0;
-        }
+        head = getWearImage(headEquipment, bareHead);
+        chest = getWearImage(chestEquipment, bareChest);
+        legs = getWearImage(legsEquipment, bareLegs);
+        shield = getWearImage(shieldEquipment, 0);
+        weapon = getWearImage(weaponEquipment, 0);
     }
 }
