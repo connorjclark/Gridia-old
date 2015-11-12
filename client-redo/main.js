df8d479fe891ed78f4e39d9522497742135d85ed
Connor Clark <cjamcl@gmail.com>
2015-11-11 21:20:35 -0600

    remove old phaser stuff...

--- client-redo/main.js
+++ client-redo/main.js
@@ -13,7 +13,35 @@ var itemsConfig;
 var music;
 var tileSize = 32;
 var chunkSize = 20;
+var viewPort;
 
+// TODO see if pooling helps at all
+
+function getParameterByName(name, defaultValue) {
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+      results = regex.exec(location.search);
+  return results === null ? defaultValue : decodeURIComponent(results[1].replace(/\+/g, " ")) || defaultValue;
+}
+
+var SpritePool = (function() {
+  var sprites = [];
+
+  return {
+    retire: function(sprite) {
+      sprites.push(sprite);
+    },
+    get: function() {
+      if (sprites.length) {
+        return sprites.pop();
+      } else {
+        return new PIXI.Sprite();
+      }
+    }
+  };
+})();
+
+// indexes into spritesheets given item/floor/player types
 var TileIndexer = (function() {
   return {
     getFloor: function(floorType, x, y) {
@@ -23,11 +51,11 @@ var TileIndexer = (function() {
       if (floorType === 1) {
         index = useTemplate(0, 1, x, y);
         var tileSheetIndex = (index / 100) | 0;
-        baseTexture = PIXI.utils.TextureCache["assets/templates/templates" + tileSheetIndex + ".png"];
+        baseTexture = PIXI.utils.BaseTextureCache["assets/templates/templates" + tileSheetIndex + ".png"];
         index = index % 100;
       } else {
         var tileSheetIndex = (floorType / 100) | 0;
-        baseTexture = PIXI.utils.TextureCache["assets/floors/floors" + tileSheetIndex + ".png"];
+        baseTexture = PIXI.utils.BaseTextureCache["assets/floors/floors" + tileSheetIndex + ".png"];
         index = floorType % 100;
       }
 
@@ -47,33 +75,38 @@ var TileIndexer = (function() {
       }
 
       var tileSheetIndex = (index / 100) | 0;
-      var baseTexture = PIXI.utils.TextureCache["assets/items/items" + tileSheetIndex + ".png"];
+      var baseTexture = PIXI.utils.BaseTextureCache["assets/items/items" + tileSheetIndex + ".png"];
       index = index % 100;
       var rect = new PIXI.Rectangle((index%10)*tileSize, ((index/10)|0)*tileSize, width, height);
 
       return new PIXI.Texture(baseTexture, rect);
+    },
+    getPlayer: function(playerType) {
+      // TODO
     }
   };
 })();
 
 function createFloor(x, y, floor) {
-  var sprite = new PIXI.Sprite(TileIndexer.getFloor(floor, x, y));
+  var sprite = SpritePool.get();
+  sprite.texture = TileIndexer.getFloor(floor, x, y);
   sprite.x = x * tileSize;
   sprite.y = y * tileSize;
   return sprite;
 }
 
 function createItem(x, y, item) {
-  var sprite = new PIXI.Sprite(TileIndexer.getItem(item, globalTick));
+  var sprite = SpritePool.get();
+  sprite.texture = TileIndexer.getItem(item, globalTick);
   sprite.x = x * tileSize;
   sprite.y = y * tileSize - sprite.height + tileSize; // offset for items taller than one tile
   return sprite;
 }
 
-var ChunkManager = (function() {
+var Map = (function(width, height) {
   var chunks = {};
 
-  // particle container draw sprites very quickly
+  // particle containers draw sprites very quickly
   // but all children must have the same base texture
   // so, one particle container per tilesheet
   var floorParticleContainers = [];
@@ -84,6 +117,7 @@ var ChunkManager = (function() {
   
   function addToParticleContainers(particleContainers, sprite, layer) {
     if (!particleContainers[sprite.texture.baseTexture.imageUrl]) {
+      // TODO there is a limit to how many sprites this can render....
       particleContainers[sprite.texture.baseTexture.imageUrl] = new PIXI.ParticleContainer();
       layer.addChild(particleContainers[sprite.texture.baseTexture.imageUrl]);
     }
@@ -114,7 +148,7 @@ var ChunkManager = (function() {
         floorParticleContainers[floor.texture.baseTexture.imageUrl].removeChild(floor);
 
         // update data
-        floor.texture = TileIndexer.getFloor(this.data[x][y].floor, x, y);
+        floor.texture = TileIndexer.getFloor(this.data[x % chunkSize][y % chunkSize].floor, x, y);
 
         // place in correct particle container
         if (!floorParticleContainers[floor.texture.baseTexture.imageUrl]) {
@@ -148,7 +182,8 @@ var ChunkManager = (function() {
 
         for (var i = 0; i < chunkSize; i++) {
           for (var j = 0; j < chunkSize; j++) {
-            if (itemsConfig[this.data[i][j].item.type].animations.length > 1) {
+            if (itemsConfig[this.data[i][j].item.type]
+                && itemsConfig[this.data[i][j].item.type].animations.length > 1) {
               this.redrawItem(i, j);
             }
           }
@@ -182,86 +217,137 @@ var ChunkManager = (function() {
     return chunk;
   }
 
-  return {
-    getChunk: function(x, y) {
-      var chunk = chunks[x + ',' + y];
+  function getChunk(x, y) {
+    var chunk = chunks[x + ',' + y];
 
-      if (!chunk) {
-        chunk = loadChunk(x, y);
+    if (!chunk && inBounds(x * chunkSize, y * chunkSize)) {
+      chunk = loadChunk(x, y);
+    }
+
+    return chunk;
+  }
+
+  function cull(minX, maxX, minY, maxY) {
+    $.each(chunks, function(key, chunk) {
+      var inViewport = chunk.x >= minX && chunk.x <= maxX && chunk.y >= minY && chunk.y <= maxY;
+      
+      if (!inViewport) {
+        console.log('culling', chunk.x, chunk.y);
+
+        $.each(chunk.floors, function() {
+          $.each(this, function() {
+            removeFloor(this);
+            SpritePool.retire(this);
+          });
+        });
+        $.each(chunk.items, function() {
+          $.each(this, function() {
+            removeItem(this);
+            SpritePool.retire(this);
+          });
+        });
+
+        delete chunks[key];
       }
+    });
+  }
 
-      return chunk;
-    },
-    cull: function(minX, maxX, minY, maxY) {
-      $.each(chunks, function(key, chunk) {
-        var inViewport = chunk.x >= minX && chunk.x <= maxX && chunk.y >= minY && chunk.y <= maxY;
+  function inBounds(x, y) {
+    return x >= 0 && y >= 0 && x < width && y < height;
+  }
+
+  function getChunkWithTile(x, y) {
+    var chunkX = (x / chunkSize) | 0;
+    var chunkY = (y / chunkSize) | 0;
+    return chunks[chunkX + ',' + chunkY];
+  }
+
+  function getTile(x, y) {
+    // TODO ... why am I doing this again?
+    if (!inBounds(x, y)) {
+      return {floor:1, item:{type:0}};
+    }
+
+    var chunkX = (x / chunkSize) | 0;
+    var chunkY = (y / chunkSize) | 0;
+    var chunk = chunks[chunkX + ',' + chunkY];
+    return chunk && chunk.data.length && chunk.data[0].length ? chunk.data[x % chunkSize][y % chunkSize] : {floor:1, item:{type:0}};
+  }
+
+  function getFloor(x, y) {
+    return getTile(x, y).floor;
+  }
+
+  function getItem(x, y) {
+    return getTile(x, y).item;
+  }
+
+  function setTile(x, y, tile) {
+    // TODO
+  }
+
+  function setFloor(x, y, floor) {
+    if (inBounds(x, y)) {
+      var chunk = getChunkWithTile(x, y);
+      if (chunk) {
+        var isWater = floor === 1;
+        var wasWater = chunk.data[x % chunkSize][y % chunkSize].floor === 1;
         
-        if (!inViewport) {
-          console.log('culling', chunk.x, chunk.y);
-
-          $.each(chunk.floors, function() {
-            $.each(this, function() {
-              removeFloor(this);
-              this.destroy();
-            });
-          });
-          $.each(chunk.items, function() {
-            $.each(this, function() {
-              removeItem(this);
-              this.destroy();
-            });
-          });
-
-          delete chunks[key];
+        chunk.data[x % chunkSize][y % chunkSize].floor = floor;
+        
+        // if water, then we should redraw all neighboring tiles
+        if (isWater || wasWater) {
+          for (var i = -1; i <= 1; i++) {
+            for (var j = -1; j <= 1; j++) {
+              var chunk2 = getChunkWithTile(x + i, y + j);
+              if (chunk2 && inBounds(x + i, y + j)) {
+                chunk2.redrawFloor(x + i, y + j);
+              }
+            }
+          }
+        } else {
+          chunk.redrawFloor(x, y);
         }
-      });
-    },
-    chunks: chunks,
+      }
+    }
+  }
+
+  function setItem(x, y, item) {
+    // TODO
+  }
+
+  return {
+    getTile: getTile,
+    getFloor: getFloor,
+    getItem: getItem,
+    setTile: setTile,
+    setFloor: setFloor,
+    setItem: setItem,
     floorLayer: floorLayer,
-    itemLayer: itemLayer
+    itemLayer: itemLayer,
+    cull: cull,
+    getChunk: getChunk
   };
-})();
+})(chunkSize * 15, chunkSize * 15);
 
 function updateChunks() {
-  var minChunkX = (view.x / tileSize / chunkSize) | 0;
-  var maxChunkX = ((view.x + window.innerWidth) / tileSize / chunkSize) | 0;
-  var minChunkY = (view.y / tileSize / chunkSize) | 0;
-  var maxChunkY = ((view.y + window.innerHeight) / tileSize / chunkSize) | 0;
+  var earlyLoading = 1;
 
-  // console.log(minChunkX, maxChunkX, minChunkY, maxChunkY);
-
-  // hard code size of map
-  minChunkX = Math.max(minChunkX, 0);
-  minChunkY = Math.max(minChunkY, 0);
-  maxChunkX = Math.min(maxChunkX, 14);
-  maxChunkY = Math.min(maxChunkY, 14);
+  var minChunkX = ((view.x / tileSize / chunkSize) | 0) - earlyLoading;
+  var maxChunkX = (((view.x + viewPort.width) / tileSize / chunkSize) | 0) + earlyLoading;
+  var minChunkY = ((view.y / tileSize / chunkSize) | 0) - earlyLoading;
+  var maxChunkY = (((view.y + viewPort.height) / tileSize / chunkSize) | 0) + earlyLoading;
 
   for (var x = minChunkX; x <= maxChunkX; x++) {
     for (var y = minChunkY; y <= maxChunkY; y++) {
-      var chunk = ChunkManager.getChunk(x, y); // this will load it if necessary
-      if (previousGlobalTick !== globalTick) {
+      var chunk = Map.getChunk(x, y); // this will load it if necessary
+      if (chunk && previousGlobalTick !== globalTick) {
         chunk.redrawAnimations();
       }
     }
   }
 
-  ChunkManager.cull(minChunkX-1, maxChunkX+1, minChunkY-1, maxChunkY+1);
-}
-
-function getTile(x, y) {
-  if (x < 0 || y < 0) return {floor:1, item:{type:0}};
-  var chunkX = (x / chunkSize) | 0;
-  var chunkY = (y / chunkSize) | 0;
-  var chunk = ChunkManager.chunks[chunkX + ',' + chunkY];
-  return chunk && chunk.data.length && chunk.data[0].length ? chunk.data[x % chunkSize][y % chunkSize] : {floor:1, item:{type:0}};
-}
-
-function getFloor(x, y) {
-  return getTile(x, y).floor;
-}
-
-function getItem(x, y) {
-  return getTile(x, y).item;
+  Map.cull(minChunkX-earlyLoading, maxChunkX+earlyLoading, minChunkY-earlyLoading, maxChunkY+earlyLoading);
 }
 
 function keyboard(keyCode) {
@@ -302,14 +388,22 @@ function keyboard(keyCode) {
 }
 
 $(function() {
-  var renderer = PIXI.autoDetectRenderer(256, 256);
+  var renderer = PIXI.autoDetectRenderer();
+
+  if (getParameterByName('fullscreen', false)) {
+    setFullscreen();
+  } else {
+    setViewport(getParameterByName('viewx', 30) * tileSize, getParameterByName('viewy', 25) * tileSize); 
+  }
+
+  $(window).resize(function () {
+    if (getParameterByName('fullscreen', false)) {
+      setFullscreen();
+    }
+  });
 
   document.body.appendChild(renderer.view);
-
-  renderer.view.style.position = "absolute"
-  renderer.view.style.display = "block";
   renderer.autoResize = true;
-  renderer.resize(window.innerWidth, window.innerHeight);
 
   stage = new PIXI.Container();
 
@@ -331,14 +425,29 @@ $(function() {
       right = keyboard(39),
       down = keyboard(40);
 
+  function setViewport(width, height) {
+    viewPort = {width: width, height: height};
+    renderer.resize(width, height);
+  }
+
+  function setFullscreen() {
+    setViewport(window.innerWidth, window.innerHeight);
+  }
+
   function setup() {
+    $(renderer.view).click(function(e) {
+      var x = ((e.offsetX + view.x) / tileSize) | 0;
+      var y = ((e.offsetY + view.y) / tileSize) | 0;
+      Map.setFloor(x, y, Map.getFloor(x, y) === 1 ? 21 : 1); // toggle water/grass
+    });
+
     itemsConfig = PIXI.loader.resources["assets/content/items.json"].data;
 
     // hack for displating a blank image on 0 itemType. items.json is set to show a '?'
     itemsConfig[0].animations = [1];
 
-    stage.addChild(ChunkManager.floorLayer);
-    stage.addChild(ChunkManager.itemLayer);
+    stage.addChild(Map.floorLayer);
+    stage.addChild(Map.itemLayer);
 
     requestAnimationFrame(update);
   }
@@ -349,21 +458,22 @@ $(function() {
     previousGlobalTick = globalTick;
     globalTick = Math.floor(Date.now() / 1000 * 5);
 
+    var speed = 4;
     if (right.isDown) {
-      view.x += 4;
+      view.x += speed;
     }
     if (left.isDown) {
-      view.x -= 4;
+      view.x -= speed;
     }
     if (down.isDown) {
-      view.y += 4;
+      view.y += speed;
     }
     if (up.isDown) {
-      view.y -= 4;
+      view.y -= speed;
     }
 
-    ChunkManager.floorLayer.x = ChunkManager.itemLayer.x = -view.x;
-    ChunkManager.floorLayer.y = ChunkManager.itemLayer.y = -view.y;
+    Map.floorLayer.x = Map.itemLayer.x = -view.x;
+    Map.floorLayer.y = Map.itemLayer.y = -view.y;
     
     updateChunks();
 
@@ -379,10 +489,10 @@ function useTemplate(templateId, typeToMatch, x, y, z) {
   var yu = y - 1;
   var yd = y + 1;
 
-  var above = getFloor(x, yu, z) === typeToMatch;
-  var below = getFloor(x, yd, z) === typeToMatch;
-  var left = getFloor(xl, y, z) === typeToMatch;
-  var right = getFloor(xr, y, z) === typeToMatch;
+  var above = Map.getFloor(x, yu, z) === typeToMatch;
+  var below = Map.getFloor(x, yd, z) === typeToMatch;
+  var left = Map.getFloor(xl, y, z) === typeToMatch;
+  var right = Map.getFloor(xr, y, z) === typeToMatch;
 
   var offset = templateId * 50;
   var v = (above ? 1 : 0) + (below ? 2 : 0) + (left ? 4 : 0) + (right ? 8 : 0);
@@ -393,10 +503,10 @@ function useTemplate(templateId, typeToMatch, x, y, z) {
   // this is mostly guess work. I think. I wrote this code years ago. I know it works,
   // so I just copy and pasted. Shame on me.
 
-  var upleft = getFloor(xl, yu, z) === typeToMatch;
-  var upright = getFloor(xr, yu, z) === typeToMatch;
-  var downleft = getFloor(xl, yd, z) === typeToMatch;
-  var downright = getFloor(xr, yd, z) === typeToMatch;
+  var upleft = Map.getFloor(xl, yu, z) === typeToMatch;
+  var upright = Map.getFloor(xr, yu, z) === typeToMatch;
+  var downleft = Map.getFloor(xl, yd, z) === typeToMatch;
+  var downright = Map.getFloor(xr, yd, z) === typeToMatch;
 
   if (v == 15)
   {
