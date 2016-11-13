2d5fa80c7cfe7b264618e66ce86863d7e66bb444
Connor Clark <cjamcl@gmail.com>
2016-11-13 08:35:44 -0600

    markup for chatview

---
+++ client/Assets/Views/ChatView.cs
@@ -0,0 +1,21 @@
+namespace MarkLight.UnityProject
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using Gridia;
+
+    using MarkLight;
+    using MarkLight.ValueConverters;
+    using MarkLight.Views;
+    using MarkLight.Views.UI;
+
+    using UnityEngine;
+
+    public class ChatView : UIView
+    {
+
+    }
+}
